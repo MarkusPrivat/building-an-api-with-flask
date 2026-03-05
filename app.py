069224178a61f71eaf7a6310625336be07085f82
Markus <25428460+MarkusPrivat@users.noreply.github.com>
2026-03-05 09:55:36 +0100

    books dict as globel variable

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 from flask import Flask, jsonify, request
 
-
 app = Flask(__name__)
+
+# Our list of books
+books = [
+    {"id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
+    {"id": 2, "title": "1984", "author": "George Orwell"}
+]
 
 
 @app.route('/api/books', methods=['GET', 'POST'])
-def books():
-    if request.method == 'GET':
-        # Handle the GET request
-        # For now, we'll return a static list
-        books = [
-            {"id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
-            {"id": 2, "title": "1984", "author": "George Orwell"}
-        ]
-        return jsonify(books)
-
-    elif request.method == 'POST':
-        # Handle the POST request
-        # For now, we'll just return the data the user sent
-        # Later, we'll add code to save the new book in our data storage
+def handle_books():
+    if request.method == 'POST':
+        # Get the new book data from the client
         new_book = request.get_json()
+
+        # Generate a new ID for the book
+        new_id = max(book['id'] for book in books) + 1
+        new_book['id'] = new_id
+
+        # Add the new book to our list
+        books.append(new_book)
+
+        # Return the new book data to the client
         return jsonify(new_book), 201
+    else:
+        # Handle the GET request
+        return jsonify(books)
 
 
 if __name__ == "__main__":
